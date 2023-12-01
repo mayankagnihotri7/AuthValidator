4a2a53dcf9b357e41cfe9d97dfb5079a27eeda4d
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2023-12-01 13:22:45 +0530

    Fix Toastr not working

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import Signup from "components/Authentication/Signup";
 
 const App = () => (
   <Router>
+    <ToastContainer />
     <Switch>
       <Route exact path="/" render={() => <div>Home</div>} />
       <Route exact path="/about" render={() => <div>About</div>} />
